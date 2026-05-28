ba6c8609f5e67fe87821864e2bd0be0e1079ceda
wiktoriarhlk <rychlakwiktoria@gmail.com>
2026-05-28 20:36:14 +0200

    Initial project setup

---
+++ src/test/java/com/budget/personalbudgetapi/PersonalBudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.personalbudgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
